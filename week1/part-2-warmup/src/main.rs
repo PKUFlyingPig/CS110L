41091db52f83028457d1cb7c31a2ddb9b204cd41
FlyingPig <1800012735@pku.edu.cn>
2021-08-23 17:22:11 +0800

    part1 & part2 finished

--- week1/part-2-warmup/src/main.rs
+++ week1/part-2-warmup/src/main.rs
@@ -7,15 +7,34 @@ fn main() {
 }
 
 fn add_n(v: Vec<i32>, n: i32) -> Vec<i32> {
-    unimplemented!()
+    let mut retv = Vec::new();
+    for i in v.iter() {
+        retv.push(i + n);
+    }
+    retv
 }
 
 fn add_n_inplace(v: &mut Vec<i32>, n: i32) {
-    unimplemented!()
+    for i in 0..v.len() {
+        v[i] = v[i] + n;
+    }
 }
 
 fn dedup(v: &mut Vec<i32>) {
-    unimplemented!()
+    let mut dict = HashSet::new();
+    let mut newv = Vec::new();
+    for i in 0..v.len() {
+        if dict.contains(&v[i]) {
+            continue;
+        } else {
+            dict.insert(v[i]);
+            newv.push(v[i]);
+        }
+    }
+    v.clear();
+    for i in 0..newv.len() {
+        v.push(newv[i]);
+    }
 }
 
 #[cfg(test)]
