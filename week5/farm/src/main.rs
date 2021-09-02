fc5f93a117e4d1b2fb2d8f4453701fa6923530b9
FlyingPig <1800012735@pku.edu.cn>
2021-09-02 11:01:33 +0800

    week5: multithreading

--- week5/farm/src/main.rs
+++ week5/farm/src/main.rs
@@ -8,7 +8,6 @@ use std::{env, process, thread};
 /// Determines whether a number is prime. This function is taken from CS 110 factor.py.
 ///
 /// You don't need to read or understand this code.
-#[allow(dead_code)]
 fn is_prime(num: u32) -> bool {
     if num <= 1 {
         return false;
@@ -25,7 +24,6 @@ fn is_prime(num: u32) -> bool {
 /// from CS 110 factor.py.
 ///
 /// You don't need to read or understand this code.
-#[allow(dead_code)]
 fn factor_number(num: u32) {
     let start = Instant::now();
 
@@ -52,7 +50,6 @@ fn factor_number(num: u32) {
 }
 
 /// Returns a list of numbers supplied via argv.
-#[allow(dead_code)]
 fn get_input_numbers() -> VecDeque<u32> {
     let mut numbers = VecDeque::new();
     for arg in env::args().skip(1) {
@@ -66,17 +63,41 @@ fn get_input_numbers() -> VecDeque<u32> {
     numbers
 }
 
+fn pull_number_from_queue(numbers: &mut Arc<Mutex<VecDeque<u32>>>) -> Option<u32> {
+    let mut numbers_ref = numbers.lock().unwrap();
+    return numbers_ref.pop_front();
+}
+fn worker_thread(mut numbers: Arc<Mutex<VecDeque<u32>>>) {
+    loop {
+        if let Some(number) = pull_number_from_queue(&mut numbers) {
+            factor_number(number);
+        } else {
+            break;
+        }
+    }
+}
 fn main() {
     let num_threads = num_cpus::get();
     println!("Farm starting on {} CPUs", num_threads);
     let start = Instant::now();
 
     // TODO: call get_input_numbers() and store a queue of numbers to factor
+    let numbers = Arc::new(Mutex::new(get_input_numbers()));
 
     // TODO: spawn `num_threads` threads, each of which pops numbers off the queue and calls
     // factor_number() until the queue is empty
+    let mut threads = Vec::new();
+    for _ in 0..num_threads {
+        let numbers_ref = numbers.clone();
+        threads.push(thread::spawn(move || {
+            worker_thread(numbers_ref);
+        }));
+    }
 
     // TODO: join all the threads you created
+    for handle in threads {
+        handle.join().expect("Panic occurred in thread");
+    }
 
     println!("Total execution time: {:?}", start.elapsed());
 }
